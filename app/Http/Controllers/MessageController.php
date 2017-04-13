fc93906bc0277279bd58142de6e66c84ba295b23
Kerwin Bonn <kerwin.manisan@link365.net>
2017-04-13 21:49:23 +0800

    fix: add display messages.

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -11,11 +11,19 @@ use Auth;
 class MessageController extends Controller
 {
     function store(Request $request) {
+
         $message = new Message;
         $message->user_id = auth::user()->id;
         $message->message = $request->message;
         $message->save();
 
         return Response::json($message);
+    }
+
+    function index(){
+
+        $messages = Message::whereUser_id(Auth::user()->id)->get();
+
+        return Response::json($messages);
     }
 }
